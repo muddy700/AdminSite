33193b98f844b9d4f1f6bd3164d018a9bb7be2dc
Muddy700 <mohamedmfaume700@gmail.com>
2020-11-21 13:47:34 -0800

    Set Initial Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
-import { Button } from 'antd'
+import { DashboardOutlined , UserAddOutlined , TeamOutlined , UserOutlined , SettingOutlined , MenuFoldOutlined , MenuUnfoldOutlined } from '@ant-design/icons';
+import { Layout , Menu , Breadcrumb } from 'antd'
+import React , { useState } from 'react'
 import './index.css'
 
+const { Header, Content, Footer, Sider } = Layout
+const { SubMenu } = Menu
+
   const  App = () => {
-  return (
-    <div className="container">
-      <h1>Welcome</h1>
-      <Button type="primary">Button</Button>
-    </div>
-  );
+
+    const [collapsed , setCollapsed ] = useState(false)
+
+    const toggle = () => {
+      setCollapsed(!collapsed)
+    }
+
+   return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} trigger={null}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<DashboardOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="2" icon={<UserAddOutlined />}>
+              Add User
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<TeamOutlined />} title="View Users">
+              <Menu.Item key="3">All Users</Menu.Item>
+              <Menu.Item key="4">By Id</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="5" icon={<UserOutlined />}>
+              Profile
+            </Menu.Item>
+            <Menu.Item key="6" icon={<SettingOutlined />}>
+              Change Password
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} >
+            {
+              React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                onClick: toggle,
+              })
+            }
+          </Header>
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Footer Goes Here</Footer>
+        </Layout>
+      </Layout>
+    )
 }
 
 export default App;
